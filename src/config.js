9853ca1352f603e5105690391a35351828981f89
priynashu-choubey <priyanshuchoubey145@gmail.com>
2025-06-04 21:59:10 +0530

    updated config.js

--- src/config.js
+++ src/config.js
@@ -1,26 +1,26 @@
 module.exports = {
-  email: 'brittany.chiang@gmail.com',
+  email: 'priyanshuchoubey145@gmail.com',
 
   socialMedia: [
     {
       name: 'GitHub',
-      url: 'https://github.com/bchiang7',
+      url: 'https://github.com/priynashu-choubey',
     },
     {
       name: 'Instagram',
-      url: 'https://www.instagram.com/bchiang7',
+      url: 'https://www.instagram.com/priyanshuchoubeyyy/',
     },
     {
       name: 'Twitter',
-      url: 'https://twitter.com/bchiang7',
+      url: 'https://x.com/Choubey145',
     },
     {
       name: 'Linkedin',
-      url: 'https://www.linkedin.com/in/bchiang7',
+      url: 'www.linkedin.com/in/choubeypriyanshu',
     },
     {
       name: 'Codepen',
-      url: 'https://codepen.io/bchiang7',
+      url: 'https://codepen.io/Priyanshu-Choubey-the-sans',
     },
   ],
 
